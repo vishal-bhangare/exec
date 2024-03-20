d190160bc33070c27ab8af6ed349d8a8febe3565
vishalBhangare <vishalbhangare2@gmail.com>
2024-03-20 10:34:57 +0530

    basic app with expo-typescript

--- app/App.tsx
+++ app/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is my todo list :) </Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
